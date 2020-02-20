0cea49d59f7bf5977b930efec2dbc58f3918434e
ajengrf <ajengrf.12@gmail.com>
2020-02-20 15:31:04 +0700

    setting

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+// define the home page route
+router.get('/', function (req, res) {
+  res.send('Tabok Nyamok')
+})
+
+
+module.exports = router
