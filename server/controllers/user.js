35e3df7297554e8f3ddc615be317e3a67beb506b
ajengrf <ajengrf.12@gmail.com>
2020-02-20 16:43:11 +0700

    routing update user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,6 +6,7 @@ class UserController {
       name: req.body.name,
       score: req.body.score
     };
+    console.log(user, '< ini user');
     User.create(user)
       .then(result => {
         res.status(201).json(result);
@@ -19,6 +20,27 @@ class UserController {
     User.findAll()
       .then(users => {
         res.status(200).json(users);
+      })
+      .catch(err => {
+        next(err);
+      });
+  }
+
+  static update(req, res, next) {
+    let user = {
+      name: req.body.name,
+      score: req.body.score
+    };
+    let id = req.params.id;
+
+    User.update(user, {
+      where: {
+        id: id
+      },
+      returning: true
+    })
+      .then(result => {
+        res.status(200).json(result[1][0]);
       })
       .catch(err => {
         next(err);
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/user');
 
 router.post('/', userController.create);
 router.get('/', userController.findAll);
+router.put('/:id', userController.update);
 
 module.exports = router;
