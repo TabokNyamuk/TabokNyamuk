3c7c7e44528de44814f791e859a57a3027cd4eef
marcel <marcell.maruli021@gmail.com>
2020-02-21 14:42:27 +0700

    Finishing

--- client/src/main.js
+++ client/src/main.js
@@ -6,7 +6,7 @@ import io from 'socket.io-client';
 
 Vue.config.productionTip = false;
 
-Vue.prototype.$socket = io.connect('http://172.16.19.81:8080/');
+Vue.prototype.$socket = io.connect('http://localhost:3000');
 
 new Vue({
   router,
